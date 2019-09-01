c40bcc1a23ec99db922f70603d3cbc47a11d31a7
Wendler Eis <wendler_eis@hotmail.com>
2019-09-01 12:03:06 -0300

    feat: add atomic folder structure

---
+++ packages/client/src/components/pages/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <li :key="candidate.id" v-for="candidate in candidates">{{ candidate.name }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      candidates: []
+    }
+  },
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    get: function() {
+      fetch('http://localhost:3000/candidates', {
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      })
+        .then(response => response.json())
+        .then(json => {
+          this.candidates = json
+        })
+    }
+  },
+  mounted: function() {
+    this.get()
+  }
+}
+</script>
+
+<style scoped>
+</style>
