a13f8a092b21e5471f5721c7029075b2ae0ae1a5
Wendler Eis <wendler_eis@hotmail.com>
2019-09-02 20:56:12 -0300

    feat: add button atom

---
+++ packages/client/src/components/atoms/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="button">
+    <img :src="icon" :alt="`icon of button`" />
+    <p>{{text}}</p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    onClick: Function,
+    icon: String
+  }
+}
+</script>
+
+<style scoped>
+.button {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid #e0e0e0;
+  background-color: #fff;
+  padding: 8px;
+}
+
+.button img {
+  width: 24px;
+  height: 24px;
+}
+</style>
