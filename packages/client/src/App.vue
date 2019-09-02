a4013675a6eb0971a636a516616db0b8f9a3b612
Wendler Eis <wendler_eis@hotmail.com>
2019-09-01 23:35:17 -0300

    feat: add default page styles

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -17,12 +16,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: Roboto, Helvetica;
+    box-sizing: border-box;
+  }
 </style>
