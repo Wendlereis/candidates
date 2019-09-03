bb1e3168cde35a2d91ac92a5f5191818e29bb80b
Wendler Eis <wendler_eis@hotmail.com>
2019-09-02 22:11:34 -0300

    feat: add dropdownbutton molecule

---
+++ packages/client/src/components/molecules/DropdownButton.vue
@@ -0,0 +1,60 @@
+<template>
+  <span class="dropdown-button">
+    <Button :icon="require('../../assets/dots.svg')" v-on:click="toggleItems" />
+    <ul class="dropdown-button__list" v-show="showItems">
+      <li
+        class="dropdown-button__list-item"
+        v-for="item in items"
+        :key="item.text"
+        @click="item.action(item.text)"
+      >{{item.text}}</li>
+    </ul>
+  </span>
+</template>
+
+<script>
+import Button from '../atoms/Button'
+
+export default {
+  name: 'DropdownButton',
+  data: function() {
+    return {
+      showItems: false
+    }
+  },
+  props: {
+    items: Array
+  },
+  components: {
+    Button
+  },
+  methods: {
+    toggleItems: function() {
+      this.showItems = !this.showItems
+    }
+  }
+}
+</script>
+
+<style scoped>
+.dropdown-button {
+  display: block;
+  width: 100%;
+  max-width: 150px;
+}
+
+.dropdown-button__list {
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+}
+
+.dropdown-button__list-item {
+  background: #f0f0f0;
+  padding: 8px;
+}
+
+.dropdown-button__list-item:hover {
+  background: #e0e0e0;
+}
+</style>
