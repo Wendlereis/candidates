bbdfa4864279c81dcf7ca64c11c542644f064e17
Wendler Eis <wendler_eis@hotmail.com>
2019-09-04 23:03:25 -0300

    feat: add list of candidates

--- packages/client/src/components/pages/Candidates.vue
+++ packages/client/src/components/pages/Candidates.vue
@@ -2,26 +2,19 @@
   <section class="candidates">
     <section class="candidates__favorites">
       <h2>Favorites</h2>
-      <Card
-        :key="candidate.id"
-        v-for="candidate in candidates"
-        :candidate="candidate"
-      />
+      <Card :key="candidate.id" v-for="candidate in candidates" :candidate="candidate" />
     </section>
 
     <section class="candidates__available">
       <h2>Candidates</h2>
-      <Card
-        :key="candidate.id"
-        v-for="candidate in candidates"
-        :candidate="candidate"
-      />
+      <Card :key="candidate.id" v-for="candidate in candidates" :candidate="candidate" />
     </section>
   </section>
 </template>
 
 <script>
 import Card from '../organisms/Card'
+import CandidateService from '../../services/candidates'
 
 export default {
   data: function() {
@@ -37,32 +30,27 @@ export default {
     Card
   },
   methods: {
-    get: function() {
-      fetch('http://localhost:3000/candidates', {
-        mode: 'cors',
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        }
+    getCandidates: async function() {
+      const candidateService = new CandidateService()
+
+      const { data: candidates } = await candidateService.get()
+
+      candidates.sort((current, next) => {
+        var currentName = current.name
+        var nextName = next.name
+
+        if (currentName < nextName) return -1
+
+        if (currentName > nextName) return 1
+
+        return 0
       })
-        .then(response => response.json())
-        .then(json => {
-          json.sort((current, next) => {
-            var currentName = current.name
-            var nextName = next.name
 
-            if (currentName < nextName) return -1
-
-            if (currentName > nextName) return 1
-
-            return 0
-          })
-
-          this.candidates = json
-        })
+      this.candidates = candidates
     }
   },
   mounted: function() {
-    this.get()
+    this.getCandidates()
   }
 }
 </script>
